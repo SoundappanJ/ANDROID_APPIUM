b6a7776d2a06317d5299424ccbe5694f02aa2087
SoundappanJ <138235485+SoundappanJ@users.noreply.github.com>
2025-06-25 08:49:26 +0530

    second commit

--- src/main/java/android_appium_calculator/Calculator.java
+++ src/main/java/android_appium_calculator/Calculator.java
@@ -1,68 +1,206 @@
 package android_appium_calculator;
 
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.pagefactory.AndroidBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.chrono.Era;
 
 public class Calculator {
-    public static AndroidDriver driver;// static global reference variable
-    public static void Perform_Calculation() throws MalformedURLException {
-        System.out.println("=====Open the Calculator app using the Appium=====");
 
+    public static AndroidDriver driver;
+    public static String resultText;
 
-        //set the desired capabilities
-
-        DesiredCapabilities desiredCapabilities= new DesiredCapabilities();
-        DesiredCapabilities caps = new DesiredCapabilities();
-
-
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("appium:automationName", "UiAutomator2");
-        caps.setCapability("appium:deviceName", "Realme");
-        caps.setCapability("appium:udid", "EUJR5P7TEMBEFUXS");
-        caps.setCapability("appium:platformVersion", "14");
-        caps.setCapability("appium:appPackage", "com.coloros.calculator");
-        caps.setCapability("appium:appActivity", "com.android.calculator2.Calculator");
+    public static boolean openCalculatorApp() {
+        try {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("platformName", "Android");
+            caps.setCapability("appium:automationName", "UiAutomator2");
+            caps.setCapability("appium:deviceName", "Realme");
+            caps.setCapability("appium:udid", "EUJR5P7TEMBEFUXS");
+            caps.setCapability("appium:platformVersion", "14");
+            caps.setCapability("appium:appPackage", "com.coloros.calculator");
+            caps.setCapability("appium:appActivity", "com.android.calculator2.Calculator");
 
 // Prevent permissions issues
-        caps.setCapability("appium:ignoreHiddenApiPolicyError", true);//SecurityException: Permission denial for hidden_api_policy.
-        //Prevents Appium from failing when trying to bypass hidden API restrictions. Setting this to true makes Appium ignore errors related to these restrictions, avoiding crashes during session startup.
-
-        caps.setCapability("appium:enforceAppInstall", false);//Setting this to false prevents reinstallation every time. This speeds up your test startup and avoids installation errors (especially when the device has restrictions).
+       caps.setCapability("appium:ignoreHiddenApiPolicyError", true);//SecurityException: Permission denial for hidden_api_policy.
+//        //Prevents Appium from failing when trying to bypass hidden API restrictions. Setting this to true makes Appium ignore errors related to these restrictions, avoiding crashes during session startup.
+//
+caps.setCapability("appium:enforceAppInstall", false);//Setting this to false prevents installation every time. This speeds up your test startup and avoids installation errors (especially when the device has restrictions).
         caps.setCapability("appium:noReset", true);//Prevents clearing app data before starting the test session. Your app opens in the same state it was last left in
         caps.setCapability("appium:fullReset", false);
+//
+//
+        URL url = new URL("http://127.0.0.1:4723"); //no need to add remote path, because the appium version is 2
+//
 
+            driver = new AndroidDriver(url, caps);
+            System.out.println("Calculator App Launched.");
+            return true;
+        } catch (Exception e) {
+            System.out.println("Failed to launch app: " + e.getMessage());
+            return false;
+        }
+    }
 
+    public static  void clickAddition(){
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Add\"]")).click();
+    }
 
+    public static void clickSubtraction(){
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Subtract\"]")).click();
 
+    }
 
-        URL url= new URL("http://127.0.0.1:4723/"); //no need to add remote path, because the appium version is 2
+    public static void clickMultiplication(){
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Multiply\"]")).click();
 
+    }
 
-        //initialize driver
-        driver= new AndroidDriver(url,caps);
-        System.out.println("========Successfully opened the Calculator app using appium======");
-        System.out.println("====Perform the Actions=====");
-        WebElement eraseAll=driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Clear\"]"));
-        WebElement one= driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_1\"]"));
-        WebElement plus=driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Add\"]"));
-        WebElement two= driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_2\"]"));
-        WebElement equalsTo= driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Equals\"]"));
+    public static void clickDivision(){
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Divide\"]")).click();
 
-        eraseAll.click();
-        one.click();
-        plus.click();
-        two.click();
-        equalsTo.click();
+    }
 
+    public static void clickEquals(){
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Equals\"]")).click();
 
-        System.out.println("====Performed Action");
+    }
 
+    public static void clickDecimal(){
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc='Point']")).click();
+    }
 
+    public static void EraseAll(){
+        try {
+            WebElement eraseAll = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Clear\"]"));
+            eraseAll.click();
+        } catch (Exception e) {
+            System.out.println("No previous input to clear.");
+        }
+
+    }
+
+    public static String  getresultText(){
+        WebElement result = driver.findElement(By.xpath("//android.widget.TextView[@resource-id=\"com.coloros.calculator:id/result\"]"));
+        resultText = result.getText();
+        System.out.println("Result Displayed: " + resultText);
+        return resultText;
+
+    }
+
+    public static void performAddition() {
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_1\"]")).click();
+        clickAddition();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_2\"]")).click();
+        clickEquals();
+        getresultText();
+      }
+
+    public static void performSubtraction(){
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_4\"]")).click();
+        clickSubtraction();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_3\"]")).click();
+       clickEquals();
+       getresultText();
+
+    }
+
+    public static void performMultiplication(){
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_7\"]")).click();
+        clickMultiplication();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_5\"]")).click();
+
+        clickEquals();
+       getresultText();
+    }
+
+    public static void performDivision(){
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_6\"]")).click();
+        clickDivision();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_2\"]")).click();
+        clickEquals();
+        getresultText();
+
+    }
+
+    public  static void performDivisionByZero(){
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_5\"]")).click();
+       clickDivision();
+       driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_0\"]")).click();
+       clickEquals();
+        getresultText();
+
+    }
+
+    public static void performClearText(){
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_5\"]")).click();
+       clickAddition();
+       driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_4\"]")).click();
+       clickEquals();
+        getresultText();
+    }
+
+    public static void performChainedOperation(){
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_9\"]")).click();
+        clickAddition();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_6\"]")).click();
+        clickSubtraction();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_2\"]")).click();
+        clickMultiplication();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_4\"]")).click();
+        clickDivision();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_7\"]")).click();
+        clickEquals();
+        getresultText();
+    }
+
+    public static void performClickBackspace(){
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_4\"]")).click();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_7\"]")).click();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_8\"]")).click();
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Backspace\"]")).click();
+        getresultText();
+    }
+
+    public static void performDecimalNumberOperation(){
+        EraseAll();
+
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_1\"]")).click();
+        clickDecimal();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_5\"]")).click();
+        clickAddition();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_3\"]")).click();
+        clickDecimal();
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id=\"com.coloros.calculator:id/digit_8\"]")).click();
+       clickEquals();
+       getresultText();
+    }
+
+    public static void quitApp() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("App Closed.");
+        }
     }
 }
--- src/test/java/android_appium_calculator/CalculatorTest.java
+++ src/test/java/android_appium_calculator/CalculatorTest.java
@@ -1,81 +1,95 @@
 package android_appium_calculator;
 
-import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
-public class CalculatorTest extends Calculator {
-
-    public AndroidDriver driver;
+public class CalculatorTest {
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
         System.out.println("===== Setting up Appium Calculator Test =====");
-
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("appium:automationName", "UiAutomator2");
-        caps.setCapability("appium:deviceName", "Realme");
-        caps.setCapability("appium:udid", "EUJR5P7TEMBEFUXS");
-        caps.setCapability("appium:platformVersion", "14");
-        caps.setCapability("appium:appPackage", "com.coloros.calculator");
-        caps.setCapability("appium:appActivity", "com.android.calculator2.Calculator");
-
-
-        // Prevent permissions issues
-        caps.setCapability("appium:ignoreHiddenApiPolicyError", true);//SecurityException: Permission denial for hidden_api_policy.
-        //Prevents Appium from failing when trying to bypass hidden API restrictions. Setting this to true makes Appium ignore errors related to these restrictions, avoiding crashes during session startup.
-
-        caps.setCapability("appium:enforceAppInstall", false);//Setting this to false prevents installation every time. This speeds up your test startup and avoids installation errors (especially when the device has restrictions).
-        caps.setCapability("appium:noReset", true);//Prevents clearing app data before starting the test session. Your app opens in the same state it was last left in
-        caps.setCapability("appium:fullReset", false);
-
-
-        URL url = new URL("http://127.0.0.1:4723/"); //no need to add remote path, because the appium version is 2
-
-        driver = new AndroidDriver(url, caps);
-        System.out.println("=== Calculator App Launched ===");
+        boolean isAppOpened = Calculator.openCalculatorApp();
+        Assert.assertTrue(isAppOpened, "App failed to open");
     }
 
-    @Test
-    public void testAddition() {
-        System.out.println("=== Running Test: 1 + 2 ===");
-
-        try {
-            WebElement eraseAll = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Clear\"]"));
-            eraseAll.click();
-        } catch (Exception e) {
-            System.out.println("No previous input to clear.");
-        }
-
-        WebElement one = driver.findElement(By.id("com.coloros.calculator:id/digit_1"));
-        WebElement plus = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Add\"]"));
-        WebElement two = driver.findElement(By.id("com.coloros.calculator:id/digit_2"));
-        WebElement equals = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Equals\"]"));
-
-        one.click();
-        plus.click();
-        two.click();
-        equals.click();
-
-        WebElement result = driver.findElement(By.id("com.coloros.calculator:id/result"));
-        String resultText = result.getText();
-
-        System.out.println("Actual Result: " + resultText);
-        Assert.assertEquals(resultText.trim(), "3", "Result should be 3");
+    @Test(priority = 1)
+    public void testAddition() throws MalformedURLException {
+        System.out.println("=== TC_01: Addition Test===");
+        Calculator.performAddition();
+        Assert.assertEquals(Calculator.getresultText().trim(), "3", "Result should be 3");
     }
+
+    @Test(priority = 2)
+    public void testSubtraction() {
+        System.out.println("===TC_02:  Subtraction Test===");
+        Calculator.performSubtraction();
+        Assert.assertEquals(Calculator.getresultText().trim(),"1","Result should be 1");
+    }
+
+    @Test(priority = 3)
+    public void testMultiplication() {
+        System.out.println("===TC_03: Multiplication  Test===");
+        Calculator.performMultiplication();
+        Assert.assertEquals(Calculator.getresultText().trim(),"35","Result should be 35");
+    }
+
+    @Test(priority = 4)
+    public void testDivision() {
+        System.out.println("===TC_04: Division  Test===");
+        Calculator.performDivision();
+        Assert.assertEquals(Calculator.getresultText().trim(),"3","Result should be 3");
+    }
+
+    @Test(priority = 5)
+    public void testDivisionByZero() {
+        System.out.println("===TC_05: Division By Zero  Test===");
+        Calculator.performDivisionByZero();
+        Assert.assertEquals(Calculator.getresultText().trim(),"Cannot divide by 0","Mismatch of Text");
+    }
+
+    @Test(priority = 6)
+    public void testClearText() {
+        System.out.println("===TC_06: Clear Text Test===");
+        Calculator.performClearText();
+        Assert.assertEquals(Calculator.getresultText().trim(),"","Result should be Null or empty space");
+    }
+
+    @Test(priority = 7)
+    public void testChainedOperation() {
+        System.out.println("===TC_07:  Chained Operation  Test===");
+        Calculator.performChainedOperation();
+        Assert.assertEquals(Calculator.getresultText().trim(),"13.857142857142","Result Mismatch");
+    }
+
+
+    @Test(priority = 8)
+    public void testBackSpace() {
+        System.out.println("===TC_08:  BackSpace  Test===");
+        Calculator.performClickBackspace();
+        Assert.assertEquals(Calculator.getresultText().trim(),"473","Result Mismatch");
+    }
+
+
+    @Test(priority = 9)
+    public void testDecimalpoint() {
+        System.out.println("===TC_09:  TestDecimal===");
+        Calculator.performDecimalNumberOperation();
+        Assert.assertEquals(Calculator.getresultText().trim(),"5.3","Result Mismatch");
+    }
+
+
+
+
+
+
+
+
+
 
     @AfterClass
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-            System.out.println("=== Closed Calculator App ===");
-        }
+        Calculator.quitApp();
     }
 }
